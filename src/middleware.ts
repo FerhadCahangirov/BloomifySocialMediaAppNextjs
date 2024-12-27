db45e407ccb27421901e00f936623a5a7fd3b8f7
Ferhad Cahangirov <ferhad.cahangirov002@gmail.com>
2024-12-28 02:15:20 +0400

    Integrate Clerk components to project and fix version problems

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, ClerkMiddlewareAuth, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/settings(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/settings(.*)", "/"]);
 
 export default clerkMiddleware((auth: ClerkMiddlewareAuth, req: NextRequest) => {
     if (isProtectedRoute(req)) {
-        auth.protect();
+        auth().protect();
     }
 });
 
