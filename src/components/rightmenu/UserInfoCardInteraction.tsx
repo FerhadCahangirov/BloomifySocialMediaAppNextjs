ec89a3cee337caa3c2d9a2af774040081d6d6173
Ferhad Cahangirov <ferhad.cahangirov002@gmail.com>
2024-12-31 15:12:10 +0400

    Implemented server actions block, follow and integrated it with the user card interactions component. Additionally, utilized useOptimistic to handle changes optimistically, improving user experience.

--- src/components/rightmenu/UserInfoCardInteraction.tsx
+++ src/components/rightmenu/UserInfoCardInteraction.tsx
@@ -1,3 +1,6 @@
+import { switchBlock, switchFollow } from "@/lib/action";
+import { useOptimistic, useState } from "react";
+
 const UserInfoCardInteraction = ({
     userId,
     isUserBlocked,
@@ -9,21 +12,63 @@ const UserInfoCardInteraction = ({
     isFollowing: boolean;
     isFollowingSent: boolean;
 }) => {
+
+    const [userState, setUserState] = useState({
+        following: isFollowing,
+        blocked: isUserBlocked,
+        followingRequestSent: isFollowingSent
+    });
+
+    const follow = async () => {
+        switchOptimisticState("follow");
+        try {
+            await switchFollow(userId);
+
+            setUserState(prev => ({
+                ...prev,
+                following: prev.following && false,
+                followingRequestSent: !prev.following && !prev.followingRequestSent ? true : false
+            }))
+        } catch (err) { }
+    }
+
+    const [optimisticState, switchOptimisticState] = useOptimistic(
+        userState, (state, value: "follow" | "block") => (value === "follow" ? {
+            ...state,
+            following: state.following && false,
+            followingRequestSent: !state.following && !state.followingRequestSent ? true : false
+        } : { ...state, blocked: !state.blocked })
+    )
+
+    const block = async () => {
+        switchOptimisticState("block");
+
+        try {
+            await switchBlock(userId);
+            setUserState(prev => ({
+                ...prev,
+                blocked: !prev.blocked
+            }))
+
+        } catch (err) {
+
+        }
+    }
     return (
         <>
-            <form >
+            <form action={follow}>
                 <button className="w-full bg-blue-500 text-white text-sm rounded-md p-2">
-                    {isFollowing
+                    {optimisticState.following
                         ? "Following"
-                        : isFollowingSent
+                        : optimisticState.followingRequestSent
                             ? "Friend Request Sent"
                             : "Follow"}
                 </button>
             </form>
-            <form className="self-end ">
+            <form className="self-end" action={block} >
                 <button>
                     <span className="text-red-400 text-xs cursor-pointer">
-                        {isUserBlocked ? "Unblock User" : "Block User"}
+                        {userState.blocked ? "Unblock User" : "Block User"}
                     </span>
                 </button>
             </form>
